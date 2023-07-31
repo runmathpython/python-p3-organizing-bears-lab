b5d7fd57b39829bfe1c25e60dbbfd35d129d5fd1
Seong Kim <runmath@gmail.com>
2023-07-30 22:50:21 -0600

    test done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 10, "M", "black", "funny", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 11, "M", "brown", "quiet", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 12, "F", "grey", "cheerful", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 11, "M", "black", "noisy", 0);
+INSERT INTO bears VALUES (5,"Melissa", 7, "F", "light brown", "playful", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "brown", "gentle", 1);
+INSERT INTO bears VALUES (7,"Wendy", 8, "F", "black", "quite", 1);
+INSERT INTO bears VALUES (8,null, 14, "M", "brown", "noisy", 0);
